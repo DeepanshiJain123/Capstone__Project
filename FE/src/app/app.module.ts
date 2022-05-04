aa0a0f2339576528ee425f7d0d2076d006a1a190
Deepanshi_Jain12 <deepanshi.jain@gmail.com>
2022-05-04 16:07:10 +0530

    mmmmmmm

--- FE/src/app/app.module.ts
+++ FE/src/app/app.module.ts
@@ -4,6 +4,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+
+
+
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { FormsModule } from '@angular/forms';
 import { AdminModule } from './admin/admin.module';
@@ -16,9 +19,13 @@ import { SignupComponent } from './authentication/signup/signup.component';
   ],
   imports: [
     BrowserModule,
+
     AppRoutingModule,
+
     FormsModule,
     AdminModule
+    
+
   ],
   providers: [],
   bootstrap: [AppComponent]
